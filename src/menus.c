99ff0d7a428c01db01a526b788950f2c969b207a
QuaternionWake <simep@proton.me>
2024-03-11 15:47:15 +0100

    Changed exit flag to goto

--- src/menus.c
+++ src/menus.c
@@ -21,7 +21,6 @@ void mainMenu() {
     int gridSize = 4;
     prerenderMenu(selection, gridSize);
     while (1) {
-        int exit = 0;
         qw_input input = getInput();
         switch(input){
         case INPUT_UP:    if (selection>0)                     selection--; break;
@@ -33,14 +32,13 @@ void mainMenu() {
             case 0: initializeGame(gridSize, 5);   break;
             case 1: printf("\033[H\033[J\n\tScore saving coming soom I pwomise :3\n"); while(getInput() == NO_INPUT); break;
             case 2: printf("\033[H\033[J\n\tNothing here yet :(\n"); while(getInput() == NO_INPUT); break;
-            case 3: exit = 1;   break;
+            case 3: goto Exit;
             } break;
-        case INPUT_EXIT:    exit = 1; break;
+        case INPUT_EXIT:  goto Exit;
         default: continue;
         }
-        if (exit)
-            break;
         prerenderMenu(selection, gridSize);
     }
+    Exit:
     resetMenuRenderingGlobals();
 }
